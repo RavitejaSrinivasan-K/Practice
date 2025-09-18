e41e78f336ae51b2eab4f4090a4f5520fe3c2c85
Raviteja K <123230823+RavitejaSrinivasan-K@users.noreply.github.com>
2025-09-18 08:52:56 +0530

    Learning Path Of JavaScript

--- JavaScript-Programs/Day5-Statement.js
+++ JavaScript-Programs/Day5-Statement.js
@@ -83,3 +83,111 @@ for(let a=1; a<=10; a++)
 }
 
 
+
+//PRACTICE - 2
+
+//IF, ELSE, ELSE IF, SWITCH, BREAK, CONTINUE
+
+
+let voteAge = 18
+if(voteAge > 23){
+    console.log("ELIGIBLE")
+
+    if(voteAge < 25){   //NESTED IDF CONDITION
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+
+}
+
+
+
+else if(voteAge > 23){
+    console.log("NOT ELIGIBLE")
+}
+else if(voteAge === '18')
+{
+    console.log('YES RYT')
+}
+else{
+    console.log("Sorry!")
+}
+
+
+
+//SWITCH
+console.log('---SWITCH---')
+
+let day = 60
+switch(day){
+    case 1 : console.log("Monday")
+    break
+    case 2 : console.log("Tuesday")
+    break
+    case 3 : console.log("Wednesday")
+    break
+    case 4 : console.log("Thursday")
+    break
+    case 5 : console.log("Friday")
+    break
+    default : console.log("Happy its weekend!")
+    break
+      
+}
+
+
+
+
+//LOOPS 
+console.log('---FOR---')
+//initialization ; condition ; increment / decrement
+for(let i=1; i<=5; i++){
+    console.log(i)
+}
+
+
+
+//WHILE
+//initialization 
+//  condition 
+//  increment / decrement
+console.log('---WHILE---')
+
+let j = 1
+while(j >= 5)
+{
+     j++
+    console.log(j)
+   
+}
+
+
+//DO- WHILE
+ console.log('---DO-WHILE---')
+//initialization 
+//  increment / decrement
+//   condtion
+
+let k =1
+do{
+    console.log(k)
+      k++
+}
+while(k >= 5)
+
+
+
+
+ console.log('---CONTINUE---')
+
+
+ for(let a=1 ; a<=10 ; a++){
+    if(a == 4){
+        continue
+    }
+    console.log(a)
+ }
+
+
