075eedf2022b4c9e35a56ec6ade722c0f18ea438
Raviteja K <123230823+RavitejaSrinivasan-K@users.noreply.github.com>
2025-09-18 08:52:00 +0530

    Learning Path Of JavaScript

--- JavaScript-Programs/Day4-Operators.js
+++ JavaScript-Programs/Day4-Operators.js
@@ -101,3 +101,137 @@ console.log(x)
 var a = 0
 var b = 20
 console.log(a^b)
+
+
+
+//PRACTICE - 2
+// OPERATERS
+
+//ARRITHMENTIC
+var a = 10
+var b = 20
+console.log("ADD :" , a + b)
+
+console.log("SUB :" , a - b)   //+10-20 = -10
+
+console.log("MUL:" , a * b) 
+
+console.log("DIV :" , a / b)   //QUOTIENT
+
+console.log("MOD :" , a % b)   //REMINDER
+
+console.log("ROOT :" , a ** b )    //ROOT
+
+
+
+
+//ASSINGMENT
+
+var a = 100
+var b = 10
+
+a += b    // a = 100 + 10 => 110
+console.log(a)
+
+a -= b    //a = 110 - 10 => 100
+console.log(a)
+
+a *= b     // a = 100 * 10 => 1000
+console.log(a)
+
+a /= b       // a = 1000 / 10 => 100 
+console.log(a)
+
+a **= b       // a = 100 / 10 => 100000000000000000000
+console.log(a)
+
+
+
+//COMPARISON
+
+var a = "wer"
+var b = 'wer'
+console.log(  a == b )   //Only checking value
+
+console.log(  a === b  )   //Stritcly checking value
+
+
+var a = 123
+var b = 321
+console.log( a < b )
+
+console.log( a > b )
+
+var a = 100
+var b = 100
+
+console.log( a >= b )
+console.log( a <= b )
+
+var a = 10
+var b = '10'
+console.log(!a)
+console.log( a != b )
+console.log( a !== b )
+
+
+console.log("---LOGICAL---")
+
+
+//LOGICAL
+
+// &&-AND   , ||-OR   ,  !-NOT
+
+let age = 18
+if(age == '18'  &&  age < 18)
+{
+    console.log('Outstanding')
+}
+else{
+    console.log("Poor Perfomance")
+}
+
+
+if(age == '18'  ||  age < 18)
+{
+    console.log('Outstanding')
+}
+else{
+    console.log("Poor Perfomance")
+}
+
+
+
+//UNARY
+//POST INCREMENT / DECREMENT
+
+var a = 100
+console.log( a++ )
+console.log( a )
+console.log( a )
+console.log( a-- , a )
+
+
+//PRE INCREMENT / DECREMENT
+
+var b = 200
+console.log( ++b , b )
+
+console.log( --b , b )
+
+
+
+
+//TERNARY / CONDITIONAL OPERATER => ?
+age = 18
+if(age < 23){
+    console.log("Eligible")
+}
+else{
+    console.log("Not Elegible")
+}
+
+
+age = (age < 23)? true : false
+console.log(age)
+
